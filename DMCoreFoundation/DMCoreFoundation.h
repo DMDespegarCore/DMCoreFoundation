71d5077fac02390fa9630aa5e90a3e6b3b54fac9
Nicolas Miyasato <nicolas.miyasato@despegar.com>
2015-07-15 15:55:19 -0300

    Initial Commit

---
+++ DMCoreFoundation/DMCoreFoundation.h
@@ -0,0 +1,19 @@
+//
+//  DMCoreFoundation.h
+//  DMCoreFoundation
+//
+//  Created by Nicolas Miyasato on 7/15/15.
+//  Copyright (c) 2015 Nicolas Miyasato. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DMCoreFoundation.
+FOUNDATION_EXPORT double DMCoreFoundationVersionNumber;
+
+//! Project version string for DMCoreFoundation.
+FOUNDATION_EXPORT const unsigned char DMCoreFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DMCoreFoundation/PublicHeader.h>
+
+
